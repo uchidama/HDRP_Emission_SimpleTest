2069428dad79a9a73387d636be3067918ea9dc5d
Uchida Masatoshi <shinishimaru@gmail.com>
2020-07-19 02:40:45 +0900

    first commit

---
+++ Assets/Scripts/AppManage.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AppManage : MonoBehaviour {
+
+	public GameObject ball;
+	
+	// Use this for initialization
+	void Start () {
+
+		makeBall ();
+	
+	}
+
+	void makeBall(){
+
+		if(ball == null)
+        {
+			return;
+        }
+
+		// make rand pos
+		Vector3 pos = new Vector3 (Random.Range (-4.0f, 4.0f), Random.Range (3.0f, 8.0f), Random.Range (-4.0f, 4.0f) );
+		GameObject b = (GameObject)Instantiate(ball,
+		            pos,
+		            Quaternion.identity);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.frameCount % 5 == 0){
+			makeBall ();
+		}
+
+	}
+	
+}
---
+++ Assets/Scripts/EmissiveSphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+public class EmissiveSphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var c_r = UnityEngine.Random.Range(0.0f, 1.0f);
+        var c_g = UnityEngine.Random.Range(0.0f, 1.0f);
+        var c_b = UnityEngine.Random.Range(0.0f, 1.0f);
+        var col = (Color)((Vector4)(new float4(c_r, c_g, c_b, 1)));
+        var _intensity = 75000.0f;
+
+        this.GetComponentInChildren<Renderer>().material.SetColor("_EmissiveColor", col * _intensity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
